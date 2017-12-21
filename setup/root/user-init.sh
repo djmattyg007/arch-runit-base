c9d7647e6b6706e581239441356977a2bb4f0654
Matthew Gamble <git@matthewgamble.net>
2017-12-22 00:01:42 +1100

    More testing

--- setup/root/user-init.sh
+++ setup/root/user-init.sh
@@ -7,16 +7,21 @@ set -e
 groupadd -g 99 nobody
 groupadd -g 100 users
 
-# Add user "nobody" to primary group "users" (will remove any other group membership)
-usermod -g users nobody
-
-# Add user "nobody" to secondary group "nobody" (will retain primary membership)
-usermod -a -G nobody nobody
-
-# Setup env for user nobody
-mkdir -p /home/nobody
+# The nobody user has also been removed from default Arch installs
+useradd -g users -G nobody -s /usr/bin/nologin -u 99 nobody
 chown -R nobody:users /home/nobody
 chmod -R 775 /home/nobody
 
+# Add user "nobody" to primary group "users" (will remove any other group membership)
+#usermod -g users nobody
+
+# Add user "nobody" to secondary group "nobody" (will retain primary membership)
+#usermod -a -G nobody nobody
+
+# Setup env for user nobody
+#mkdir -p /home/nobody
+#chown -R nobody:users /home/nobody
+#chmod -R 775 /home/nobody
+
 # Ensure home directory is set for user "nobody"
-usermod -d /home/nobody nobody
+#usermod -d /home/nobody nobody
